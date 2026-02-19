e7daba6c71fe648c852f1b0c7efabf0a2f75fbd7
zheny <zhenyakrylov96@gmail.com>
2026-02-19 09:58:07 -0500

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MedicationsCat"
+include(":app")
+include(":core:common")
+include(":core:model")
+include(":core:database")
+include(":core:network")
+include(":core:data")
+include(":feature:auth")
+include(":feature:medications")
+include(":feature:profile")
